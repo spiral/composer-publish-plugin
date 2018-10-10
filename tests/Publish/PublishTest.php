1755fc5912994c62737ec6d18d535f8f6f107654
Wolfy-J <wolfy.jd@gmail.com>
2018-10-10 17:48:23 +0300

    less verbose

---
+++ tests/Publish/PublishTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Composer\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Composer\Publish;
+
+class PublishTest extends TestCase
+{
+    public function testParseEnsure()
+    {
+        $p = Publish::parse('@public/dir', 'ensure');
+
+        $this->assertSame('@public/dir', $p->getSource());
+        $this->assertSame('@public/dir', $p->getTarget());
+        $this->assertSame(null, $p->getMode());
+        $this->assertSame('ensure', $p->getType());
+    }
+}
