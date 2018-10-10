306620e8123cfcdbd7d1f75b51bd830bf647c194
Wolfy-J <wolfy.jd@gmail.com>
2018-10-10 13:44:39 +0300

    publish

---
+++ src/Publish.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Composer;
+
+class Publish
+{
+    /** @var string */
+    private $target;
+
+    /** @var null|string */
+    private $source;
+
+    /** @var string */
+    private $type;
+
+    /** @var null|string */
+    private $mode;
+
+    /**
+     * @return string
+     */
+    public function getTarget(): string
+    {
+        return $this->target;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getSource(): ?string
+    {
+        return $this->source;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getMode(): ?string
+    {
+        return $this->mode;
+    }
+
+    /**
+     * @param string $path
+     * @param string $data
+     * @param string $options
+     * @return Publish
+     *
+     * @throws PublishException
+     */
+    public static function parse(string $path, string $data, string $options): Publish
+    {
+        $publish = new static();
+
+        if (strpos($data, ':') !== false) {
+            list($publish->source, $publish->target) = explode(':', $data);
+        } else {
+            $publish->target = $data;
+        }
+
+        if (!empty($publish->source)) {
+            $publish->source = rtrim($path, '/') . '/' . $publish->source;
+        }
+
+        if (strpos($options, ':') !== false) {
+            list($publish->type, $publish->mode) = explode(':', $options);
+            self::validateMode($publish->mode);
+
+        } else {
+            $publish->type = $options;
+        }
+        self::validateType($publish->type);
+
+        return $publish;
+    }
+
+    /**
+     * @param string $type
+     *
+     * @throws PublishException
+     */
+    private static function validateType(string $type)
+    {
+        if (!in_array($type, ['follow', 'replace', 'ensure'])) {
+            throw new PublishException("Invalid operation type `{$type}`.");
+        }
+    }
+
+    /**
+     * @param string $mode
+     *
+     * @throws PublishException
+     */
+    private static function validateMode(string $mode)
+    {
+        if (!in_array($mode, ['runtime', 'readonly'])) {
+            throw new PublishException("Invalid file mode `{$mode}`.");
+        }
+    }
+}
--- src/PublishPlugin.php
+++ src/PublishPlugin.php
@@ -9,12 +9,20 @@
 namespace Spiral\Composer;
 
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
+use Symfony\Component\Process\Process;
 
-class PublishPlugin implements PluginInterface
+class PublishPlugin implements PluginInterface, EventSubscriberInterface
 {
+    // Extra key to point to publish command handler
+    public const PUBLISH_CMD = 'publish-cmd';
+
+    // Extra key to declare files and directories to publish
+    public const PUBLISH_KEY = 'publish';
+
     /** @var Composer instance */
     private $composer;
 
@@ -38,7 +46,58 @@ class PublishPlugin implements PluginInterface
      */
     public function publishFiles(Event $event)
     {
-        print_r($event);
+        $cmd = $event->getComposer()->getPackage()->getExtra()[self::PUBLISH_CMD] ?? null;
+
+        if (empty($cmd)) {
+            $this->io->writeError('<comment>missing `publish-cmd` handler</comment>');
+            return;
+        }
+
+        $this->io->write("<info>Publishing package files using `<comment>{$cmd}</comment>`</info>");
+
+        $packages = $event->getComposer()->getRepositoryManager()->getLocalRepository()->getCanonicalPackages();
+        $installationManager = $event->getComposer()->getInstallationManager();
+
+        foreach ($packages as $package) {
+            $publish = $package->getExtra()[self::PUBLISH_KEY] ?? null;
+
+            if (empty($publish)) {
+                continue;
+            }
+
+            foreach ($publish as $data => $options) {
+                $this->publish($cmd, $installationManager->getInstallPath($package), $data, $options);
+            }
+        }
+    }
+
+    /**
+     * @param string $cmd
+     * @param string $path
+     * @param string $data
+     * @param string $type
+     */
+    protected function publish(string $cmd, string $path, string $data, string $type)
+    {
+        $publish = Publish::parse($path, $data, $type);
+
+        $p = new Process(join(' ', [
+            $cmd,
+            escapeshellarg($publish->getType()),
+            escapeshellarg($publish->getTarget()),
+            escapeshellarg($publish->getSource()),
+            escapeshellarg($publish->getMode())
+        ]));
+        $p->run();
+
+        if (!$p->isSuccessful()) {
+            $this->io->writeError($p->getErrorOutput());
+            return;
+        }
+
+        if ($this->io->isVerbose()) {
+            $this->io->write($p->getOutput());
+        }
     }
 
     /**
